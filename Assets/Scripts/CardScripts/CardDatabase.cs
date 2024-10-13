3be6928f162f9ce14155e37e04a927a9eccf7add
Kristians P <kperovs15@gmail.com>
2024-10-13 19:05:00 +0100

    Working database

--- Assets/Scripts/CardScripts/CardDatabase.cs
+++ Assets/Scripts/CardScripts/CardDatabase.cs
@@ -1,48 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class CardDatabase : MonoBehaviour
 {
     public static List<Card> cardList = new List<Card>();
 
-    private void Awake()
+    void Awake()
     {
-        //Public Card has these properties Card(int Id,string CardName,int Cost, Sprite ThisCardsImage,int Attack, int Damage, int ArmourClass,int Health, bool Spell, int SpellDamage,string CardDescription)
+        Debug.Log(cardList.Count);
+        //Public Card has these properties Card(int Id,string CardName,int Cost, Sprite ThisCardsImage,int Attack, int Damage, int ArmourClass,int Health, bool Ranged, int RangedDamage,bool Spell, int SpellDamage,string CardDescription)
         //Use this code to add new cards in: 
 
-        //Human Units
+        //Human Units array of 0-9
 
         //Melee
-        cardList.Add(new Card(0, "Fighter", 1, Resources.Load<Sprite>(""), 2, 3, 13, 20, false, 0, ""));
-        cardList.Add(new Card(1, "Fighter", 2, Resources.Load<Sprite>(""), 4, 5, 15, 25, false, 0, ""));
-        cardList.Add(new Card(2, "Fighter", 3, Resources.Load<Sprite>(""), 5, 5, 17, 30, false, 0, ""));
+        cardList.Add(new Card(0, "Swordman", 1, Resources.Load<Sprite>("SwordmanCard"), 2, 3, 13, 20, false,0, false, 0, ""));//Assets/CardImages/HumanCard/SwordmanCard.png
+        cardList.Add(new Card(1, "Knight", 2, Resources.Load<Sprite>("KnightCard"), 4, 5, 15, 25, false, 0, false, 0, ""));//Assets/CardImages/HumanCard/KnightCard.png
+        cardList.Add(new Card(2, "Mounted Knight", 3, Resources.Load<Sprite>("HeadKnightmanCrad"), 5, 5, 17, 30, false, 0, false, 0, ""));//Assets/CardImages/HumanCard/HeadKnightmanCrad.png
 
         //Ranged
 
-        cardList.Add(new Card(3, "Ranger", 1, Resources.Load<Sprite>(""), 2, 3, 13, 20, false, 0, ""));
-        cardList.Add(new Card(4, "Weapon", 1, Resources.Load<Sprite>(""), 4, 6, 13, 20, false, 0, ""));
-        cardList.Add(new Card(4, "Mage", 1, Resources.Load<Sprite>(""), 5, 5, 13, 20, false, 0, ""));
+        cardList.Add(new Card(3, "Archer", 1, Resources.Load<Sprite>("ArcherCard"), 2, 0, 13, 20, true, 3, false, 0, ""));//Assets/CardImages/HumanCard/ArcherCard.png
+        cardList.Add(new Card(4, "Catapult", 1, Resources.Load<Sprite>("WeaponCard"), 4, 0, 13, 20, true, 6, false, 0, ""));//Assets/CardImages/HumanCard/WeaponCard.png
+        cardList.Add(new Card(5, "Sorcerer", 1, Resources.Load<Sprite>("MageCard"), 5, 5, 13, 20, true, 5, false, 0, ""));//Assets/CardImages/HumanCard/MageCard.png
 
-        //Human Spells
+        ////Human Spells
 
-        cardList.Add(new Card(5, "Rite", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, true, -2, "Get 2 points"));
-        cardList.Add(new Card(6, "Dragon", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, true, 0, "Destroy 1 opponent's card")); //need to add card removal from opponent
+        cardList.Add(new Card(6, "Sign", 1, Resources.Load<Sprite>("Spell1Card"), 0, 0, 0, 0, false, 0, true, -2, "Get 2 points"));//Assets/CardImages/HumanCard/Spell1Card.png
+        cardList.Add(new Card(7, "Dragon", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, false, 0, true, 0, "Destroy 1 opponent's card")); //need to add card removal from opponent //Assets/CardImages/HumanCard/Spell2Card.png
 
-        //Human Equipment
+        ////Human Equipment
 
-        cardList.Add(new Card(7, "Sword and Shield", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, false, 0, "")); //need properties for equipment (need to buff/attach card on a chosen unit)
-        cardList.Add(new Card(8, "Weapon", 1, Resources.Load<Sprite>(""), 4, 6, 13, 20, false, 0, ""));
+        cardList.Add(new Card(8, "Sword and Shield", 1, Resources.Load<Sprite>(""), 3, 0, 2, 0, false, 0, true, 0, "")); //need properties for equipment (need to buff/attach card on a chosen unit) //Assets/CardImages/HumanCard/Equipment1Card.png
+        cardList.Add(new Card(9, "Armour", 1, Resources.Load<Sprite>(""), 0, 0, 5, 0, false, 0, true, 0, ""));//Assets/CardImages/HumanCard/Equipment2Card.png
 
-        //Deamon Units
+        ////Deamon Units array of 10-19
 
-        //cardList.Add(new Card(2, "Fighter", 3, Resources.Load<Sprite>(""), 5, 5, 17, 30, false, 0, ""));
-        //cardList.Add(new Card(2, "Fighter", 3, Resources.Load<Sprite>(""), 5, 5, 17, 30, false, 0, ""));
+        cardList.Add(new Card(10, "Balthazer", 1, Resources.Load<Sprite>(""), 3, 2, 13, 20, false, 0, false, 0, ""));
+        cardList.Add(new Card(11, "Abaddon", 2, Resources.Load<Sprite>(""), 4, 3, 15, 25, false, 0, false, 0, ""));
+        cardList.Add(new Card(12, "Grunt", 3, Resources.Load<Sprite>(""), 5, 5, 17, 30, false, 0, false, 0, ""));
 
+        ////Ranged
 
-        //Deamon Spells
+        cardList.Add(new Card(13, "Lamia", 1, Resources.Load<Sprite>(""), 2, 0, 13, 20, true, 2, false, 0, ""));
+        cardList.Add(new Card(14, "Behemoth", 2, Resources.Load<Sprite>(""), 4, 0, 14, 20, true, 3, false, 0, ""));
+        cardList.Add(new Card(15, "Reaper", 3, Resources.Load<Sprite>(""), 5, 0, 16, 20, true, 6, false, 0, ""));
 
-        //Deamon Equipment
+        ////Deamon Spells
+
+        cardList.Add(new Card(16, "Rite", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, false, 0, true, -2, "Get 2 points"));
+        cardList.Add(new Card(17, "Reform Oneself", 1, Resources.Load<Sprite>(""), 0, 0, 0, 0, false, 0, true,0, ""));
+
+        ////Deamon Equipment
+
+        cardList.Add(new Card(18, "Trident Of Doom", 1, Resources.Load<Sprite>(""), 5, 0, 0, 0, false, 0, true, 0, ""));
+        cardList.Add(new Card(19, "Devil's marks", 1, Resources.Load<Sprite>(""), 0, 0, 5, 0, true, 0, false, 0, ""));
 
     }
 }
--- Assets/Scripts/CardScripts/DisplayCard.cs
+++ Assets/Scripts/CardScripts/DisplayCard.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using Unity.VisualScripting;
+
 
 public class DisplayCard : MonoBehaviour
 {
-    public List<Card> DisplayCards = new List<Card>();//have DisplayCard if something not working
+    public List<Card> displayCard = new List<Card>();
     public int displayId;
 
     public int id;
@@ -17,6 +19,7 @@ public class DisplayCard : MonoBehaviour
     public int attack;
     public int damage;
     public string cardDescription;
+    public Sprite thisCardImage;
 
     //public Text cardNameText;
     //public Text costText;
@@ -25,33 +28,55 @@ public class DisplayCard : MonoBehaviour
     public Text attackText;
     public Text damageText;
     public Text cardDescriptionText;
+    public Image cardArtImage;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        DisplayCards[0] = CardDatabase.cardList[displayId]; //have DisplayCard if something not working
+        displayCard[0] = CardDatabase.cardList[displayId];
+
+        id = displayCard[0].id;
+
+        Debug.Log(displayCard.Count);
+
+        cardName = displayCard[0].cardName;
+        cost = displayCard[0].cost;
+        health = displayCard[0].health;
+        armourClass = displayCard[0].armourClass;
+        attack = displayCard[0].attack;
+        damage = displayCard[0].damage;
+        cardDescription = displayCard[0].cardDescription;
+        thisCardImage = displayCard[0].thisCardsImage;
+
+        healthText.text = "" + health;
+        armourClassText.text = "" + armourClass;
+        attackText.text = "" + attack;
+        damageText.text = "" + damage;
+        cardDescriptionText.text = "" + cardDescription;
+        cardArtImage.sprite = thisCardImage;
+
+        Debug.Log(displayCard.Count);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        id = DisplayCards[0].id;
-        cardName = DisplayCards[0].cardName;
-        cost = DisplayCards[0].cost;
-        health = DisplayCards[0].health;
-        armourClass = DisplayCards[0].armourClass;
-        attack = DisplayCards[0].attack;
-        damage = DisplayCards[0].damage;
-        cardDescription = DisplayCards[0].cardDescription;
 
 
-        healthText.text = "" + health;
-        armourClassText.text = "" + armourClass;
-        attackText.text = ""+ attack;
-        damageText.text = ""+ damage;
-        cardDescriptionText.text = ""+cardDescription;
+    }
+    void Awake()
+    {
+        //Public Card has these properties Card(int Id,string CardName,int Cost, Sprite ThisCardsImage,int Attack, int Damage, int ArmourClass,int Health, bool Ranged, int RangedDamage,bool Spell, int SpellDamage,string CardDescription)
+        //Use this code to add new cards in: 
 
+        //Human Units array of 0-9
 
+        //Melee
+        //displayCard.Add(new Card(0, "Swordman", 1, Resources.Load<Sprite>("SwordmanCard"), 2, 3, 13, 20, false, 0, false, 0, ""));//Assets/CardImages/HumanCard/SwordmanCard.png
+        //displayCard.Add(new Card(1, "Knight", 2, Resources.Load<Sprite>("KnightCard"), 4, 5, 15, 25, false, 0, false, 0, ""));//Assets/CardImages/HumanCard/KnightCard.png
+        //displayCard.Add(new Card(2, "Mounted Knight", 3, Resources.Load<Sprite>("HeadKnightmanCrad"), 5, 5, 17, 30, false, 0, false, 0, ""));//Assets/CardImages/HumanCard/HeadKnightmanCrad.png
     }
 }
