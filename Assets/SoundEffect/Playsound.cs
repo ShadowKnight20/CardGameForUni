0b8ac9fd728f658d88584dfc3a02483595d62ce3
anawin111909 <108145027+anawin111909@users.noreply.github.com>
2024-10-17 10:44:08 +0100

    update.PT
    Picture
    Sound Effect

---
+++ Assets/SoundEffect/Playsound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsound : MonoBehaviour
+{
+    public AudioSource SoundPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlaySoundeffect()
+    {
+        SoundPlayer.Play();
+    }
+}
