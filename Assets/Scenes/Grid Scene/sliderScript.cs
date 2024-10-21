a07212365b7ad0edd3adf2d6a5348dc9cb05edfc
sophiewagstaff48 <sophiewagstaff48@gmail.com>
2024-10-21 16:08:14 +0100

    health bar
    updated health slider

---
+++ Assets/Scenes/Grid Scene/sliderScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class sliderScript : MonoBehaviour
+{
+    public Slider slider;
+    public TextMeshProUGUI sliderText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderText.text = slider.value.ToString("0");
+    }
+}
